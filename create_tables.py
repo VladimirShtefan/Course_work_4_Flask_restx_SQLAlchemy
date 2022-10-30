1d990efd6a6635c1b416ec76882d468043806fdd
VladmimirShtefan <shtefanvladimir14@gmail.com>
2022-10-30 14:50:58 +0700

    добавил psycopg2

--- create_tables.py
+++ create_tables.py
@@ -1,11 +1,22 @@
+import os
+
 from app.app import create_app
-from app.config import Config
+from app.config import Config, DevConfig, ProdConfig
 from app.dao.model.director import Director
 from app.dao.model.genre import Genre
 from app.dao.model.movie import Movie
 from app.dao.model.user import User
 from app.dao.model.user_movie import UserMovie
 from app.setup_db import db
+
+
+match os.environ.get('FLASK_ENV'):
+    case 'development':
+        app = create_app(DevConfig)
+    case 'production':
+        app = create_app(ProdConfig)
+    case _:
+        raise RuntimeError('Need to set environment variable FLASK_ENV')
 
 
 def _create_tables():
