deda59ff12295a7e93e24faf162c0bb21cb4efc4
VladmimirShtefan <shtefanvladimir14@gmail.com>
2022-11-04 23:06:12 +0700

    refactor actions

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, g
+from flask import Flask, g, request, make_response
 from sqlalchemy.exc import DBAPIError
 
 from app.config import DevConfig, ProdConfig
@@ -53,8 +53,21 @@ def create_app(config) -> Flask:
                 g.session.rollback()
             finally:
                 g.session.close()
-        header = response.headers
-        header['Access-Control-Allow-Origin'] = '*'
+        origin = request.headers.get('Origin')
+        if request.method == 'OPTIONS':
+            response = make_response()
+            response.headers.add('Access-Control-Allow-Credentials', 'true')
+            response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+            response.headers.add('Access-Control-Allow-Headers', 'x-csrf-token')
+            response.headers.add('Access-Control-Allow-Methods',
+                                 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+            if origin:
+                response.headers.add('Access-Control-Allow-Origin', origin)
+        else:
+            response.headers.add('Access-Control-Allow-Credentials', 'true')
+            if origin:
+                response.headers.add('Access-Control-Allow-Origin', origin)
+
         return response
     return application
 
