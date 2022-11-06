32a09bf6b3d8d7e8f99f52b63823b22224c4439f
VladmimirShtefan <shtefanvladimir14@gmail.com>
2022-11-06 14:21:13 +0700

    refactor CORS

--- app/app.py
+++ app/app.py
@@ -60,7 +60,7 @@ def create_app(config) -> Flask:
 
 
 def register_extensions(app: Flask):
-    CORS(app=app, allow_headers=['Content-Type', 'Authorization'], methods=['POST', 'GET', 'OPTIONS'], resources='/')
+    CORS(app=app, allow_headers=['Content-Type', 'Authorization'], methods=['GET', 'HEAD', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'])
     db.init_app(app)
     migrate.init_app(app)
     api.init_app(app)
